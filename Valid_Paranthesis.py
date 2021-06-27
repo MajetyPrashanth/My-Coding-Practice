6ee753e31ff47c1dcd7a4e738a9941ccaaa46ba1
MajetyPrashanth <58392261+MajetyPrashanth@users.noreply.github.com>
2021-06-27 15:36:05 +0530

    Update Valid_Paranthesis.py

--- Valid_Paranthesis.py
+++ Valid_Paranthesis.py
@@ -1,12 +1,12 @@
 
 s = '([]{})'
 d = {'(': 0 , ')': 0 , '{': 0 , '}': 0 , '[': 0 ,']': 0 }
-#To count brackets value
+
 for i in range(len(s)):
     if s[i] in d:
         d[s[i]] += 1
         
-#To find the closing bracket
+
 for i in range(len(s)):
     for j in range(len(s)):
         if s[i] == '(':
@@ -19,7 +19,7 @@ for i in range(len(s)):
             if s[j] == ']':
                 d[s[i]] -= 1                
 print(d)
-#to check for all 0s
+
 if d['('] == 0 and d['['] == 0 and d['{'] == 0 :
     print('True')
 else:
