f56ad118f180f0173192a0ba0a2285e894e8c60c
Prashanth Majety <58392261+MajetyPrashanth@users.noreply.github.com>
2021-08-09 20:02:12 +0530

    Dynamic nature
    Added functionality so that user can add as many nodes and edges as they want.

--- Simple_Graph.py
+++ Simple_Graph.py
@@ -1,13 +1,21 @@
 from collections import defaultdict
 graph = defaultdict(list)
 def addEdge(graph,u,v):
-  graph[u],append(v)
+    graph[u].append(v)
 def gen_edges(graph):
-  edges = []
-  for node in graph:
-    for neighbor in graph[node]:
-      edges.append((node,neighbor))
-addEdge(graph,1,2)
-addEdge(graph,1,3)
-addEdge(graph,2,3)
-print(gen_edges(graph))
+    edges = []
+    for node in graph:
+        for neighbor in graph[node]:
+            edges.append((node,neighbor))
+    print(edges)
+ch = input("\nDo you want to enter first edge (y/n) ? : ")
+while ch == 'y':
+    if ch == 'y':
+        ch = input("\nDo you want to continue adding edges (y/n) ? : ")
+        if ch == 'y':
+            node1 = input("\n Enter Node 1 : ")
+            node2 = input("\n Enter Node 2 : ")
+            addEdge(graph,node1,node2)
+        elif ch == 'n':
+            continue
+gen_edges(graph)
