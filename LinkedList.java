aa9b30a05fc3ccc7a47c7783d736462f0b4be28d
Prashanth Majety <58392261+MajetyPrashanth@users.noreply.github.com>
2021-08-12 21:31:44 +0530

    Implemented without using List class

--- LinkedList.java
+++ LinkedList.java
@@ -1,28 +1,52 @@
 import java.util.*;
-public class LLprac 
+class SinglyLinkedList
 {
-    public static void main(String[] args) 
+class Node
     {
-    LinkedList<String> l1 = new LinkedList<>();
-    l1.add("Hello");
-    l1.add("This");
-    l1.add("Is");
-    l1.add("Prashanth");
-    System.out.println("After addition of elements :"+l1);
-    //changing an already set value
-    l1.set(1,"Hi");
-    System.out.println("\nAfter modification:"+l1);
-    //removing an element using index
-    l1.remove(1);
-    System.out.println("\nAfter removal of Hi using index: "+l1+"\n");
-    //removing an element by its value
-    l1.remove("Hello");
-    System.out.println("\nAfter removal of Hello: "+l1+"\n"); 
-    //Iterating through the linked list
-    System.out.println("\nPrinting using iteration : \n");
-    for (int i = 0; i < l1.size(); i++) 
-    { 
-            System.out.print(l1.get(i) + "\n "); 
-    } 
+        int data;
+        Node next;
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public Node head = null;
+    public Node tail = null;
+    public void add(int data)
+    {
+        Node newnode = new Node(data);
+        if(head == null)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail.next = newnode;
+            tail = newnode;
+        }
+    }
+    public void display()
+    {
+        Node current = head;
+        if(head == null)
+            System.out.println("LIST IS EMPTY !!!");
+        System.out.println("Nodes in the LIST ARE : ");
+        while(current != null)
+        {
+            System.out.print(current.data+"-->");
+            current = current.next;
+        }
+    }
+public static void main(String [] args)
+    {
+        SinglyLinkedList sl = new SinglyLinkedList();
+        sl.add(10);
+        sl.add(20);
+        sl.add(30);
+        sl.add(40);
+        //to display all items : 
+        sl.display();
     }
 }
