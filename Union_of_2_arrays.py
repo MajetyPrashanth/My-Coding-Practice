a751b5097bcfccde41b27d252323d0029ed01a13
Prashanth Majety <58392261+MajetyPrashanth@users.noreply.github.com>
2021-10-18 19:25:43 +0530

    Implemented new logic to eliminate Redundant Values
    Used a dictionary in order to avoid appending repeated values in arrays. After creating a dictionary like so, printed all the keys in the dictionary in order to get the desired outcome ( unique items in union only )

--- Union_of_2_arrays.py
+++ Union_of_2_arrays.py
@@ -2,7 +2,13 @@ print('Enter the elements of first array : ')
 a1 = list(map(int,input().split()))
 print('\nEnter the elements of first array : ')
 a2 = list(map(int,input().split()))
+d = {}
 for i in range(len(a2)):
     a1.append(a2[i])
+for i in range(len(a1)):
+    if a1[i] not in d:
+        d[a1[i]] = 0
+    d[a1[i]] += 1
+union = d.keys()
 print('\nUNION OF 2 ARRAYS IS : ')
-print(a1, end = ' ')
+print(union, end = ' ')
