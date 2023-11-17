cf37f258f9f1bf571ca27bf0ee6f7a5f2919b6de
Prashanth Majety <58392261+MajetyPrashanth@users.noreply.github.com>
2023-11-17 17:05:35 +0530

    Deleted old TwoSumProblem.py

--- TwoSumProblem.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = dict()
-        for i in range(len(nums)):
-            temp = nums[i]
-            rem = target - temp
-            if rem in d.keys():
-                return [d[rem], i]
-            d[nums[i]] = i
